bd486cd40ae89cd0f516238c2c91dd127f57fa4a
Aaniya401 <shreyarastogi1999@gmail.com>
2025-06-01 13:28:37 +0530

    Update PresentationTable.tsx

--- src/components/PresentationTable.tsx
+++ src/components/PresentationTable.tsx
@@ -1,68 +1,69 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table"
 import { titles } from "@/constants/constants"
 
-
 function PresentationTable({ filteredAgeWiseData }) {
+  return (
+    <div className="w-full overflow-x-auto">
+      <Table className="min-w-full table-auto border border-gray-300 rounded-lg shadow-md">
+        <TableHeader className="bg-gradient-to-r from-purple-100 to-pink-100">
+          <TableRow>
+            {titles.map((element, idx) => (
+              <TableHead
+                key={idx}
+                className="text-[16px] font-semibold text-gray-800 px-4 py-3 border-b border-gray-300"
+              >
+                {element.name}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
 
-    return (
-        <div className="lg:w-3/4 w-full">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        {titles.map((element) => (<TableHead className="w-[100px] text-[17px] border-3">
-                            {element.name}
-                        </TableHead>))}
-                    </TableRow>
-                </TableHeader>
-                <TableBody className="text-[17px] border-3">
-                    {filteredAgeWiseData.length === 0 ? <h1 className="p-2 text-nowrap">No data available</h1> : filteredAgeWiseData.map((element) => (<TableRow>
-                        <TableCell className="font-medium border-2">
-                            {element.age}
-                        </TableCell>
+        <TableBody className="text-[15px] text-gray-700">
+          {filteredAgeWiseData.length === 0 ? (
+            <TableRow>
+              <TableCell colSpan={6} className="text-center py-4">
+                No data available
+              </TableCell>
+            </TableRow>
+          ) : (
+            filteredAgeWiseData.map((element, rowIdx) => (
+              <TableRow
+                key={rowIdx}
+                className={`${rowIdx % 2 === 0 ? "bg-white" : "bg-gray-50"} transition duration-300 hover:bg-gray-100`}
+              >
+                <TableCell className="font-medium px-4 py-3 border">
+                  {element.age}
+                </TableCell>
 
-                        <TableCell className="border-2">
-                            <ul>
-                                {element.data1.length === 0 ? 'No Data Available' : element.data1.map((item) => (<li>{item}</li>))}
-                            </ul>
-                        </TableCell>
-
-                        <TableCell className="border-2">
-                            <ul>
-                                {element.data2.length === 0 ? 'No Data Available' : element.data2.map((item) => (<li>{item}</li>))}
-                            </ul>
-                        </TableCell>
-
-                        <TableCell className="border-2">
-                            <ul>
-                                {element.data3.length === 0 ? 'No Data Available' : element.data3.map((item) => (<li>{item}</li>))}
-                            </ul>
-                        </TableCell>
-
-                        <TableCell className="border-2">
-                            <ul>
-                                {element.data4.length === 0 ? 'No Data Available' : element.data4.map((item) => (<li>{item}</li>))}
-                            </ul>
-                        </TableCell>
-
-                        <TableCell className="border-2">
-                            <ul>
-                                {element.data5.length === 0 ? 'No Data Available' : element.data5.map((item) => (<li>{item}</li>))}
-                            </ul>
-                        </TableCell>
-
-                    </TableRow>))}
-                </TableBody>
-            </Table>
-
-        </div>
-    )
+                {[element.data1, element.data2, element.data3, element.data4, element.data5].map(
+                  (data, i) => (
+                    <TableCell key={i} className="px-4 py-3 border align-top">
+                      {data.length === 0 ? (
+                        <span className="italic text-gray-400">No Data Available</span>
+                      ) : (
+                        <ul className="list-disc list-inside space-y-1">
+                          {data.map((item, idx) => (
+                            <li key={idx}>{item}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </TableCell>
+                  )
+                )}
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
 
 export default PresentationTable
